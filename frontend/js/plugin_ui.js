f081873296b351adab1125e09e9ed93bf3a523af
freddie <dee.talipoff@gmail.com>
2022-06-19 23:35:51 +0500

    init

---
+++ frontend/js/plugin_ui.js
@@ -0,0 +1,35 @@
+﻿var background = chrome.extension.getBackgroundPage();
+var colors = {
+    "-1":"#58bc8a",
+    "0":"#ffeb3c",
+    "1":"#ff8b66"
+};
+var featureList = document.getElementById("features");
+
+chrome.runtime.sendMessage({action: "update_ui"}, function (response) {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+
+        var result = response.results[tabs[0].id];
+        var isMalicious = response.isMalicious[tabs[0].id];
+        var legitimatePercent = response.legitimatePercents[tabs[0].id];
+
+        for (var key in result) {
+            var newFeature = document.createElement("li");
+            //console.log(key);
+            newFeature.textContent = key;
+            //newFeature.className = "rounded";
+            newFeature.style.backgroundColor = colors[result[key]];
+            featureList.appendChild(newFeature);
+        }
+
+        $("#site_score").text(parseInt(legitimatePercent) + "%");
+        if (isMalicious) {
+            $("#res-circle").css("background", "#ff8b66");
+            $("#site_msg").text("Вредоносный контент!!!");
+            $("#site_score").text(parseInt(legitimatePercent) - 20 + "%");
+        }
+    });
+});
+
+
+
